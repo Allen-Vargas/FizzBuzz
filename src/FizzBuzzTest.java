93d2c6035d15ff83f1ebef1bbb80b2072afe7625
Allen Vargas <allenbfox@gmail.com>
2020-02-11 03:14:07 -0400

    Refactorizacion y 5 test pasando

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -5,9 +5,37 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
 	@Test
-	void testPrueba() {
-		String resultado = "1";
+	void testEsperaba1() {
+		String resultado = FizzBuzz.generarPara(1);
 		String esperado = "1";
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	void testEsperabaFizz() {
+		String resultado = FizzBuzz.generarPara(3);
+		String esperado = "Fizz";
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	void testEsperabaBuzz() {
+		String resultado = FizzBuzz.generarPara(5);
+		String esperado = "Buzz";
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	void testEsperabaFizzBuzz() {
+		String resultado = FizzBuzz.generarPara(15);
+		String esperado = "FizzBuzz";
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	void testEsperaba28() {
+		String resultado = FizzBuzz.generarPara(28);
+		String esperado = "28";
 		assertEquals(esperado, resultado);
 	}
 }
--- src/Program.java
+++ src/Program.java
@@ -8,36 +8,39 @@ public class Program {
 	
 class FizzBuzz {
 	
+	/* Procedimiento que genera 100 numero del 0 al 99 mostrando en patalla Fizz, Buzz o el numero*/
 	void generarMuchos() {
 		for(int i = 0; i < 100; i++)
 			{
-				if (i % 3 == 0 && i % 5 == 0)
-				{
-					System.out.println("FizzBuzz");                    
-				}
-				else
-				{
-					if(i % 3 == 0)
-					{
-						System.out.println("Fizz");
-					}
-					else
-					{
-						if(i % 5 == 0)
-						{
-							System.out.println("Buzz");
-						}
-						else
-						{
-							System.out.println(i);
-						}				
-					}	
-				}		
+				System.out.println(generarPara(i));
+					
 		    }
 	}
 	
-	String generarPara(int numero) {
-		String cadena = String.valueOf(numero); 
-		return cadena;
+	/* Funcion que devuelve Fizz, Buzz o el numero en String de un solo numero */
+	static String generarPara(int numero) {
+		
+		if (numero % 3 == 0 && numero % 5 == 0)
+		{
+			return "FizzBuzz";                  
+		}
+		else
+		{
+			if(numero % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else
+			{
+				if(numero % 5 == 0)
+				{
+					return "Buzz";
+				}
+				else
+				{
+					return String.valueOf(numero);
+				}				
+			}	
+		}	
 	}
 }
